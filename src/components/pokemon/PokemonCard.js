ec0a61c0c17f1b6dd93db58263d47f000dad8303
augusto-moraes <augui2009@gmail.com>
2019-12-05 15:41:17 -0300

    pokeballs whyle loading

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Sprite = styled.img`
     width: 5em;
     height: 5em; 
+    display: none;
 `;
 
 export default class PokemonCard extends Component {
@@ -34,11 +35,16 @@ export default class PokemonCard extends Component {
                     <div className='card-header'>
                         <h5>{this.state.pokemonIndex}</h5>
                     </div>
+                    {this.state.imageLoading ? (<img src='https://icon-library.net/images/small-pokeball-icon/small-pokeball-icon-5.jpg' style={{width:'5em', height: '5em' }} className='card-img-top rounded mx-auto d-block mt-2' />) : null }
                     <Sprite 
                         className='card-img-top rounded mx-auto mt-2' 
                         src={this.state.imageUrl} 
                         onLoad={() => this.setState({imageLoading: false})} 
                         onError={() => this.setState({tooManyRequests: true})} 
+                        style={
+                            this.state.tooManyRequests ? {display:"none"} : 
+                            this.state.imageLoading ? null : {display:"block"}
+                        }
                     />
                     <div className='card-body mx-auto'>
                         <h6 className='card-tittle'>{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ')}</h6>
