f2cc691c4a9624e52e1c5fb7284a416df9a0c275
augusto-moraes <augui2009@gmail.com>
2019-12-05 22:28:50 -0300

    animations & battle file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import NavBar from "./components/layout/NavBar";
 import Dashboard from './components/layout/Dashboard';
+import Battle from './components/battle/Battle';
 
 function App() {
   return (
-    <div className="App">
-       <NavBar />
-       <div className='container'>
-         <Dashboard />
-       </div>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route exact path='/battle' component={Battle} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+
 import styled from 'styled-components';
 
 const Sprite = styled.img`
     width: 5em;
     height: 5em; 
     display: none;
+`;
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24); 
+    transition: all 0.3s cubic-bezier(0.25,0.8,0.25,1);
+    &:hover{
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+    -moz-user-select: none;
+    -website-user-select: none;
+    user-select: none;
+    -o-user-select: none;
 `;
 
 export default class PokemonCard extends Component {
@@ -31,11 +44,21 @@ export default class PokemonCard extends Component {
     render() {
         return (
             <div className='col-md-3 col-sm-6 mb-5'>
-                <div className='card'>
+                <Card className='card'>
                     <div className='card-header'>
-                        <h5>{this.state.pokemonIndex}</h5>
+                         <img 
+                            src="https://myrealdomain.com/images/pixel-art-flame.png" 
+                            style={{width:'2em', height: '2em' }} 
+                            className='rounded d-block' 
+                        />
                     </div>
-                    {this.state.imageLoading ? (<img src='https://icon-library.net/images/small-pokeball-icon/small-pokeball-icon-5.jpg' style={{width:'5em', height: '5em' }} className='card-img-top rounded mx-auto d-block mt-2' />) : null }
+                    {this.state.imageLoading ? (
+                        <img 
+                            src="https://i.pinimg.com/originals/f9/7f/5c/f97f5c6510994f677877b08320475008.gif" 
+                            style={{width:'5em', height: '5em' }} 
+                            className='card-img-top rounded mx-auto d-block mt-2' 
+                        />
+                    ) : null }
                     <Sprite 
                         className='card-img-top rounded mx-auto mt-2' 
                         src={this.state.imageUrl} 
@@ -49,7 +72,7 @@ export default class PokemonCard extends Component {
                     <div className='card-body mx-auto'>
                         <h6 className='card-tittle'>{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ')}</h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
